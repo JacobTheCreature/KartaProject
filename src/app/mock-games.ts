736fee61108b8df8e4f11de02816be6599b1aa7d
JacobTheCreature <123994795+JacobTheCreature@users.noreply.github.com>
2024-05-20 08:20:36 -0800

    Update mock-games.ts

--- src/app/mock-games.ts
+++ src/app/mock-games.ts
@@ -20,5 +20,25 @@ export const GAMES: Game[] = [
         id: 4, 
         title: 'Rocket Bot Royale',
         url: 'https://rocketbotroyale.winterpixel.io/'
+    },
+    {
+        id: 5,
+        title: 'FPS Assault Shooter',
+        url: '//html5.gamedistribution.com/rvvASMiM/0eb473da1c0546f39e348d83219db5e7/index.html?gd_sdk_referrer_url=https%3A%2F%2Fkevin.games%2Fassault-shooter&amp;gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2tldmluLmdhbWVzL2Fzc2F1bHQtc2hvb3RlciIsInBhcmVudERvbWFpbiI6ImtldmluLmdhbWVzIiwidG9wRG9tYWluIjoia2V2aW4uZ2FtZXMiLCJoYXNJbXByZXNzaW9uIjp0cnVlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJob3N0IjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJ2ZXJzaW9uIjoiMS41LjE3In0%253D'
+    },
+    {
+        id: 6,
+        title: 'Zombsroyale.io',
+        url: 'https://zombsroyale.io/'
+    },
+    {
+        id: 7,
+        title: 'Short Life 2',
+        url: '//html5.gamedistribution.com/rvvASMiM/4882edc8ad2d43c798713f1028421379/index.html?gd_sdk_referrer_url=https%3A%2F%2Fkevin.games%2Fshort-life-2&amp;gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2tldmluLmdhbWVzL3Nob3J0LWxpZmUtMiIsInBhcmVudERvbWFpbiI6ImtldmluLmdhbWVzIiwidG9wRG9tYWluIjoia2V2aW4uZ2FtZXMiLCJoYXNJbXByZXNzaW9uIjp0cnVlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJob3N0IjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJ2ZXJzaW9uIjoiMS41LjE3In0%253D'
+    },
+    {
+        id: 8,
+        title: 'Fireboy & Watergirl 6',
+        url: '//html5.gamedistribution.com/rvvASMiM/56da8a54fe204845b34ccff750d4a60b/index.html?gd_sdk_referrer_url=https%3A%2F%2Fkevin.games%2Ffireboy-and-watergirl&amp;gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2tldmluLmdhbWVzL2ZpcmVib3ktYW5kLXdhdGVyZ2lybCIsInBhcmVudERvbWFpbiI6ImtldmluLmdhbWVzIiwidG9wRG9tYWluIjoia2V2aW4uZ2FtZXMiLCJoYXNJbXByZXNzaW9uIjp0cnVlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJob3N0IjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJ2ZXJzaW9uIjoiMS41LjE3In0%253D'
     }
 ]
