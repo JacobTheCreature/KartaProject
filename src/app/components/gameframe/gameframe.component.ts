06bf70ebbc3a490c8288ee307edb0869d819f000
JacobTheCreature <123994795+JacobTheCreature@users.noreply.github.com>
2024-05-20 08:09:40 -0800

    Update gameframe.component.ts

--- src/app/components/gameframe/gameframe.component.ts
+++ src/app/components/gameframe/gameframe.component.ts
@@ -2,19 +2,35 @@ import { Component, Input } from '@angular/core';
 import { Game } from '../../game';
 import { GameService } from '../../services/game.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { NgIf } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
   selector: 'app-gameframe',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './gameframe.component.html',
   styleUrl: './gameframe.component.css'
 })
 export class GameframeComponent {
-  @Input() game?: Game;
+  game?: Game;
 
-  constructor(public gameService: GameService, private sanitizer: DomSanitizer) {}
+  constructor(
+    public gameService: GameService,
+    private sanitizer: DomSanitizer,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const gameID = Number(this.route.snapshot.paramMap.get('id'));
+    if (gameID) {
+      this.gameService.getGame(gameID).subscribe(game => {
+        this.gameService.setSelectedGame(game);
+        this.game = game;
+      });
+    }
+  }
 
   getSanitizedUrl(url: string | undefined): SafeResourceUrl | undefined {
     return url ? this.sanitizer.bypassSecurityTrustResourceUrl(url) : undefined;
