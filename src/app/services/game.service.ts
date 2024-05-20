1c05ed388cb3bf925e8ac6c5c41ed7de5da5d596
JacobTheCreature <123994795+JacobTheCreature@users.noreply.github.com>
2024-05-20 08:16:03 -0800

    Update game.service.ts

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Game } from '../game';
-import { GAMES } from '../mock-games';
-import { Observable, of } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const options = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +13,36 @@ import { Observable, of } from 'rxjs';
 export class GameService {
   selectedGame: Game | undefined;
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   getGames(): Observable<Game[]> {
-    const games = of(GAMES);
-    return games;
+    const url = `http://localhost:3000/games`;
+    return this._http.get<Game[]>(url);
   }
 
-  getGame(game: Game): void {
+  getGame(gameID: number): Observable<Game> {
+    const url = `http://localhost:3000/game/${gameID}`;
+    return this._http.get<Game>(url).pipe(
+      tap(game => this.selectedGame = game)
+    );
+  }
+
+  setSelectedGame(game: Game): void {
     this.selectedGame = game;
+  }
+
+   createGame(game: Game): Observable<Game> {
+     const url = `http://localhost:3000/game/`;
+     return this._http.post<Game>(url, game, options);
+   }
+
+   deleteGame(gameID: number): Observable<void> {
+     const url = `http://localhost:3000/game/${gameID}`;
+     return this._http.delete<void>(url, options);
+  }
+
+   updateGame(game: Game): Observable<void> {
+    const url = `http://localhost:3000/game/${game.id}`;
+    return this._http.put<void>(url, game, options);
   }
 }
