3a4237cb99e9e370208bc92a60f037c080354753
JacobTheCreature <123994795+JacobTheCreature@users.noreply.github.com>
2024-05-21 12:45:46 -0800

    Update dropdown.component.ts

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -3,22 +3,26 @@ import { GAMES } from '../../mock-games';
 import { NgFor, NgIf } from '@angular/common';
 import { Game } from '../../game';
 import { GameService } from '../../services/game.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
   imports: [
     NgFor,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.css'
 })
 export class DropdownComponent implements OnInit{
   @Output() gameSelected = new EventEmitter<Game>();
-  
+
+  searchInput: string = '';
   
   games: Game[] = [];
+  filteredList: Game[] | undefined;
 
   constructor(private gameService: GameService) {}
 
@@ -36,18 +40,29 @@ export class DropdownComponent implements OnInit{
   updateDropdown(): void {
     this.gameService.getGames().subscribe(games => {
       this.games = games;
-      if (this.games.length > 0) {
-        const firstGameId = this.games[0].id;
-        this.gameService.getGame(firstGameId).subscribe(selectedGame => {
-          this.gameSelected.emit(selectedGame);
-        });
-      }
+      this.filteredList = games;
     });
   }
+
+  defaultGame(): void {
+    const firstGameId = this.games[0].id;
+    this.gameService.getGame(firstGameId).subscribe(selectedGame => {
+      this.gameSelected.emit(selectedGame);
+    });
+  }
+
+  filter(): void {
+    const query = this.searchInput.toLowerCase();
+    this.filteredList = this.games.filter(game =>
+      game.title.toLowerCase().includes(query)
+    );
+  }
+  
 
   ngOnInit(): void {
     this.gameService.getGames().subscribe(games => {
       this.games = games;
+      this.filteredList = games;
       if (this.games.length > 0) {
         const firstGameId = this.games[0].id;
         this.gameService.getGame(firstGameId).subscribe(selectedGame => {
