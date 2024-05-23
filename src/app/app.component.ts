8228540596be1a5b2d3b47b00605cf446166e073
JacobTheCreature <123994795+JacobTheCreature@users.noreply.github.com>
2024-05-23 08:07:21 -0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, DashboardComponent]
 })
 export class AppComponent {
   title = 'coolgames';
